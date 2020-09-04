b691c4651d4c56aede727bcd88da081530756436
Mauro <mauro.santos@marlo.com.au>
2020-09-04 12:14:01 -0300

    Adding the return of static contents

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+//Adding a middleware on the requests
 const logMiddleware =  function (req, res, next){
     console.log("The API received a request!");
     next();
 }
-
 app.use(logMiddleware);
+
+
+//Static content return
+app.use(express.static("./site"));
+
+
+//HTTP Requests
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
