2d9602b2786b25b5f282c530b2f44867327b3704
mauro santos <mauro.santos@marlo.com.au>
2020-09-04 12:02:12 -0300

    adding a sample middleware

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+const logMiddleware =  function (req, res, next){
+    console.log("The API received a request!");
+    next();
+}
+
+app.use(logMiddleware);
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
